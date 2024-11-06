a436d647c31497845bcacbdb9f7d46851ff1ac18
Tarun Kumar <tarunrajput39200@gmail.com>
2024-11-06 15:50:15 +0530

    Updated SQLLearnings.sql with new queries

--- SQLLearnings.sql
+++ SQLLearnings.sql
@@ -15,7 +15,28 @@ values
 
 select * from student;
 
+SHOW DATABASES;
 
+create database if not exists instagram;
+
+use instagram;
+
+create table user (
+
+id int,
+age int,
+name varchar(30) not null,
+email varchar(50) unique,
+followers int default 0,
+following int,
+constraint age_check check (age >= 13), primary key (id)
+);
+create table post (
+id int primary key,
+content varchar(100),
+user_id int,
+foreign key (user_id) references user(id)
+);
 
 
 
