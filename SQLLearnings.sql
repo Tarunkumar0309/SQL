97410f203d92ba8d4667a2f760350e987da3cc4e
Tarun Kumar <tarunrajput39200@gmail.com>
2024-11-06 10:11:18 +0530

    SQLLearnings.sql

--- SQLLearnings.sql
+++ SQLLearnings.sql
@@ -1,4 +1,21 @@
-create database college;
+create database University;
+
+use University;
+
+create table student(
+rollno int,
+name varchar(30),
+age int
+);
+
+insert into student
+values
+(101, "adam", 12),
+(102, "bob", 14);
+
+select * from student;
+
+
 
 
 
