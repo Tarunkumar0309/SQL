98718ff71c5b853dc156e5365305678db6027dfe
Tarun Kumar <tarunrajput39200@gmail.com>
2024-11-08 12:41:30 +0530

    Added new SQL entries

--- SQLLearnings.sql
+++ SQLLearnings.sql
@@ -39,8 +39,95 @@ values
 (3, 16, "casey", "casey3@email.com", 300, 306),
 (4, 15, "donald", "donald@gmail.com", 200, 105);
 
+insert into user
+(id, age, name, email, following)
+values
+(8, 20, "ram", "rama@yahoo.in", 14500000),
+(9, 17, "shyam", "shyam@yahoo.in", 14543200);
+
+
 -- select id, name, age from user;-- 
--- select *from user;-- 
+
+-- Where clause-- 
+
+select name , age
+from user
+-- Where followers >= 200;-- 
+Where age < 16;
+
+
+select name, age, followers
+
+from user
+
+-- where age + 1 = 17;-- 
+ 
+where age > 15 AND followers >200;
+
+select name, age, email
+from user 
+where age >14
+limit 2;
+
+select name, age, followers, following
+from user
+order by followers ASC; 
+
+SELECT MAX(FOLLOWERS)
+FROM USER;
+
+SELECT MAX(AGE)
+FROM USER;
+
+SELECT sum(followers)
+FROM user;
+
+SELECT  age, max(followers)
+FROM user
+GROUP BY age
+HAVING max(followers) >200
+ORDER BY age DESC;
+
+UPDATE user
+SET followers = 600
+WHERE age = 16;
+
+DELETE FROM user
+WHERE age =14;
+
+ALTER TABLE user
+ADD COLUMN city VARCHAR(25) DEFAULT "Delhi";
+
+ALTER TABLE user
+DROP COLUMN city;
+
+ALTER TABLE user
+RENAME TO instaUser;
+
+
+ALTER TABLE instaUser
+RENAME TO user;
+
+
+ALTER TABLE user
+CHANGE COLUMN followers subs INT DEFAULT 0;
+
+
+
+ALTER TABLE user
+MODIFY subs INT DEFAULT 5;
+SELECT *FROM user;
+
+
+
+SET SQL_SAFE_UPDATES = 0;
+
+
+
+select *from user;
+
+-- TRUNCATE TABLE user;-- 
+
 
 select distinct age from user;
 
